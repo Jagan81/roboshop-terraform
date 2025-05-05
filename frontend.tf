98b2a836d1c71a9290577c33ac2b49a0639f2c34
Jagannnnnn <jaganmohan85220@gmail.com>
2025-05-06 00:46:54 +0530

    roboshop code

--- frontend.tf
+++ frontend.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-04164efe3462b5107"]
 
   tags = {
     Name = "frontend"
