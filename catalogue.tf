c521c0fd3ef830d80053b17cc9f675bc56c6ed86
Jagannnnnn <jaganmohan85220@gmail.com>
2025-05-06 14:54:54 +0530

    roboshop code

--- catalogue.tf
+++ catalogue.tf
@@ -1,18 +1,17 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "catalogue" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-04164efe3462b5107"]
 
   tags = {
-    Name = "frontend"
+    Name = "catalogue"
   }
 }
 
-
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "catalogue" {
   zone_id = "Z0388353395L515P1D630"
-  name = "frontend-dev"
+  name = "catalogue-dev"
   type = "A"
   ttl = 10
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.catalogue.private_ip]
 }
--- mongodb.tf
+++ mongodb.tf
@@ -1,18 +1,17 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "mongodb" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-04164efe3462b5107"]
 
   tags = {
-    Name = "frontend"
+    Name = "mongodb"
   }
 }
 
-
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "mongodb" {
   zone_id = "Z0388353395L515P1D630"
-  name = "frontend-dev"
+  name = "mongodb-dev"
   type = "A"
   ttl = 10
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.mongodb.private_ip]
 }
