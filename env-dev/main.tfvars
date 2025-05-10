dca34004a749dc057eed77a69b6d819b26c2a557
Jagannnnnn <jaganmohan85220@gmail.com>
2025-05-10 10:52:50 +0530

    roboshop code

---
+++ env-dev/main.tfvars
@@ -0,0 +1,18 @@
+instances = {
+  mongodb = {
+    ami_id =  "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+  }
+  catalogue = {
+    ami_id =  "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+  }
+  frontend = {
+    ami_id =  "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+  }
+}
+zone_id = "Z0388353395L515P1D630"
+
+vpc_security_group_ids = ["sg-04164efe3462b5107"]
+env = "dev"
--- variables.tf
+++ variables.tf
@@ -1,36 +1,8 @@
-variable "ami_id" {
-  default = "ami-09c813fb71547fc4f"
-}
 
-variable "instance_type" {
-  default = "t3.small"
-}
+ variable "vpc_security_group_ids" {}
 
- variable "vpc_security_group_ids" {
-   default = ["sg-04164efe3462b5107"]
- }
+variable "instances" {}
 
-variable "instances" {
-  default = {
-    mongodb = {
-    ami_id =  "ami-09c813fb71547fc4f"
-    instance_type = "t3.small"
-    }
-    catalogue = {
-      ami_id =  "ami-09c813fb71547fc4f"
-      instance_type = "t3.micro"
-    }
-    frontend = {
-      ami_id =  "ami-09c813fb71547fc4f"
-      instance_type = "t3.micro"
-    }
-  }
-}
+variable "zone_id" {}
 
-variable "zone_id" {
-  default = "Z0388353395L515P1D630"
-}
-
-variable "env" {
-  default = "dev"
-}
+variable "env" {}
