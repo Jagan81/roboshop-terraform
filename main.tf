872e257d004928e85410b8db8b5dd028be817d87
Jagannnnnn <jaganmohan85220@gmail.com>
2025-05-10 10:39:28 +0530

    roboshop code

--- main.tf
+++ main.tf
@@ -1,35 +1,11 @@
-resource "aws_instance" "instance" {
- for_each = var.instances
-  ami           = each.value["ami_id"]
-  instance_type = each.value["instance_type"]
-  vpc_security_group_ids = var.vpc_security_group_ids
-  tags = {
-    Name = each.key
-  }
-
-}
-
-resource "aws_route53_record" "record" {
+module "ec2" {
   for_each = var.instances
+  source = "./modules/ec2"
+
+  ami_id = each.value["ami_id"]
+  env =var.env
+  instance_type =  each.value["instance_type"]
+  name = each.key
+  vpc_security_group_ids = var.vpc_security_group_ids
   zone_id = var.zone_id
-  name = "${each.key}-${var.env}"
-  type = "A"
-  ttl = 10
-  records = [aws_instance.instance[each.key].private_ip]
 }
-#
-# resource "null_resource" "catalogue" {
-#   provisioner "remote-exec" {
-#
-#     connection {
-#       type = "ssh"
-#       user = "ec2-user"
-#       password = "DevOps321"
-#       host = aws_instance.catalogue.private_ip
-#     }
-#     inline = [
-#       "Sudo pip3.11 install ansible",
-#       "ansible-pull -i localhost, -u https://github.com/jagan81/roboshop-ansible roboshop.yml -e component_name=catalogue -e env=dev,"
-#     ]
-#   }
-# }
---
+++ modules/ec2/main.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+  tags = {
+    Name = var.name
+  }
+
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = var.zone_id
+  name = "${var.name}-${var.env}"
+  type = "A"
+  ttl = 10
+  records = [aws_instance.instance.private_ip]
+}
+#
+# resource "null_resource" "catalogue" {
+#   provisioner "remote-exec" {
+#
+#     connection {
+#       type = "ssh"
+#       user = "ec2-user"
+#       password = "DevOps321"
+#       host = aws_instance.catalogue.private_ip
+#     }
+#     inline = [
+#       "Sudo pip3.11 install ansible",
+#       "ansible-pull -i localhost, -u https://github.com/jagan81/roboshop-ansible roboshop.yml -e component_name=catalogue -e env=dev,"
+#     ]
+#   }
+# }
